09a8b4f0dc5c2fc3a5eece5fbf5704341f461427
Nguyễn Minh Thiện <23521484@gm.uit.edu.vn>
2025-10-19 12:47:53 +0700

    fix response service package

--- src/main/java/com/iseeyou/fortunetelling/dto/response/servicepackage/ServicePackageResponse.java
+++ src/main/java/com/iseeyou/fortunetelling/dto/response/servicepackage/ServicePackageResponse.java
@@ -6,13 +6,14 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @SuperBuilder
 public class ServicePackageResponse {
     private String id;
-    private String seerId;
+    private SeerInfo seer;
     private String packageTitle;
     private String packageContent;
     private String imageUrl;
@@ -23,4 +24,15 @@ public class ServicePackageResponse {
     private String rejectionReason;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    @Data
+    @NoArgsConstructor
+    @SuperBuilder
+    public static class SeerInfo {
+        private UUID id;
+        private String fullName;
+        private String avatarUrl;
+        private Double avgRating;
+        private Integer totalRates;
+    }
 }
